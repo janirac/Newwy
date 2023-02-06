714636983b92d3573cec38930acb81d1b0895911
Janira Crispin <janiracrispin@gmail.com>
2023-02-06 00:06:26 -0500

    setup frontend authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,21 +32,6 @@ class ApplicationController < ActionController::API
       end
 
 
-      def test
-        if params.has_key?(:login)
-          login!(User.first)
-        elsif params.has_key?(:logout)
-          logout!
-        end
-      
-        if current_user
-          render json: { user: current_user.slice('id', 'session_token') }
-        else
-          render json: ['No current user']
-        end
-      end
-
-
     private
 
 
