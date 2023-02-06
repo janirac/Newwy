8f039614ea2827b518cbb8c6847d29d2bb70451e
Janira Crispin <janiracrispin@gmail.com>
2023-02-05 23:47:02 -0500

    csrf protection setup, backend complete

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::SessionsController < ApplicationController
     @user = current_user 
 
      if @user 
-        render json: {user: current_user} 
+        render 'api/users/show'
      else 
         render json: { user: nil }
      end
@@ -23,7 +23,7 @@ class Api::SessionsController < ApplicationController
 
       if @user&.save
          login!(@user)
-         render json: {user: @user}
+         render 'api/users/show'
       else 
          render json: {errors: ['The provided credentials were invalid'] }, status: :unauthorized
       end 
